18cfbed5d1fb7d2b48bc146cde93611257eb33bd
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-06-19 22:17:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,6 +16,6 @@ class App < Sinatra::Base
     @num = params[:number].to_i
     @phrase = params[:phrase]
     i = 0
-    i < @num ? @phrase and i++ : @phrase
+    i < @num ? @phrase AND i++ : @phrase
   end
 end
